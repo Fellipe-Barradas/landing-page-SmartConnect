4ef91336b70b40382199f4e97a93d7d1c9efdbb5
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-05-19 10:56:21 -0300

    Adicionado mais imagens do produto

--- src/layout/website/Article.tsx
+++ src/layout/website/Article.tsx
@@ -3,16 +3,12 @@ interface ArticleProps{
     className?:string
     classNameDesc?:string
     title:string
-    imgUrl:any
+   
 }
 
 export default function Article(prop:ArticleProps){
-    return (
-        <article className={`
-        ${prop.className}
-        flex p-5 items-center justify-around gap-[10rem]
-        `}>
-            <div className={`flex flex-col gap-10 ${prop.classNameDesc} w-[40rem] `}>
+    return (        
+            <div className={`flex flex-col gap-10 ${prop.classNameDesc} w-[40rem] `} > 
                 <h1 className='text-3xl text-blue-400 font-semibold'>
                     {prop.title}
                 </h1>
@@ -20,7 +16,5 @@ export default function Article(prop:ArticleProps){
                     {prop.children}
                 </p>
             </div>
-            <img src={prop.imgUrl} alt="Imagem representativa do produto" className='w-[28rem] h-[16rem] rounded-xl border border-zinc-700  flex-1'/>
-        </article>
     )
 }
--- src/layout/website/Main.tsx
+++ src/layout/website/Main.tsx
@@ -1,21 +1,64 @@
 import Area from "../Area";
-import imagem from "../.././imgs/nasa-Q1p7bh3SHj8-unsplash.jpg"
-import imagem2 from "../.././imgs/alina-grubnyak-ZiQkhI7417A-unsplash.jpg"
 import Article from "./Article";
-
+import imagem1 from "../../imgs/adi-goldstein-EUsVwEOsblE-unsplash.jpg"
+import imagem2 from "../../imgs/alina-grubnyak-ZiQkhI7417A-unsplash.jpg"
+import imagem3 from "../../imgs/benjamin-dada-EDZTb2SQ6j0-unsplash.jpg"
+import imagem4 from "../../imgs/nasa-Q1p7bh3SHj8-unsplash.jpg"
+import imagem5 from "../../imgs/sergey-zolkin-_UeY8aTI6d0-unsplash.jpg"
 export default function Main(){
     return (
-      <Area className='flex-1 text-white mt-10 flex flex-col  gap-40'>
-        <Article imgUrl={imagem} title="O que é Smart Connect?">    
-           O SmartConnect é um produto tecnológico revolucionário projetado para simplificar a conectividade e aprimorar a comunicação no mundo acelerado de hoje. Com seus recursos de ponta e interface intuitiva, o SmartConnect permite que indivíduos e empresas permaneçam conectados sem esforço, não importa onde estejam.
+      <Area className='flex-1 text-white mt-10 flex flex-col gap-40'>
+        <article className={`
+        flex p-5 items-center justify-around gap-[10rem]
+        `} >
+            <Article  title="O que é Smart Connect?">    
+                O SmartConnect é um produto tecnológico revolucionário projetado para simplificar a conectividade e aprimorar a comunicação no mundo acelerado de hoje. Com seus recursos de ponta e interface intuitiva, o SmartConnect permite que indivíduos e empresas permaneçam conectados sem esforço, não importa onde estejam.
+            </Article>
+            <img src={imagem1} alt="imagem representativa" className="img" />
+        </article>
+
+        <article className={`
+        flex p-5 items-center justify-around gap-[10rem] flex-row-reverse
+        `} >
+            <Article 
+                classNameDesc="items-end text-right text-lg"
+                title="Ligação instantânia">    
+                Com sua avançada tecnologia Bluetooth, o SmartConnect permite o emparelhamento instantâneo entre o dispositivo e os dispositivos compatíveis. Você pode facilmente estabelecer uma conexão sem a necessidade de configurações manuais ou longos procedimentos de emparelhamento.
+            </Article>
+            <img src={imagem2} alt="imagem representativa" className="img" />
+        </article>
+       
+        <article className={`
+        flex p-5 items-center justify-around gap-[10rem]
+        `} >
+             <Article 
+             title="Conexão compartilhada">    
+           O SmartConnect suporta a conexão de vários dispositivos simultaneamente, tornando-o ideal para profissionais e multitarefas. Você pode alternar sem esforço entre dispositivos com um único toque, permitindo a transição perfeita de um dispositivo para outro sem interrupções.
         </Article>
-        <Article 
-        className='flex-row-reverse ' 
-        imgUrl={imagem2}
+        <img src={imagem3} alt="imagem representativa" className="img" />
+        </article>
+       
+        <article className={`
+        flex p-5 items-center justify-around gap-[10rem] flex-row-reverse
+        `} >
+            <Article 
         classNameDesc="items-end text-right text-lg"
-        title="Ligação instantânia">    
-            Com sua avançada tecnologia Bluetooth, o SmartConnect permite o emparelhamento instantâneo entre o dispositivo e os dispositivos compatíveis. Você pode facilmente estabelecer uma conexão sem a necessidade de configurações manuais ou longos procedimentos de emparelhamento.
+        title="Alta velocidade de transferência de dados">    
+           Experimente velocidades de transferência de dados extremamente rápidas com o SmartConnect. Se você precisa compartilhar arquivos, transmitir mídia ou navegar na Internet, o SmartConnect garante uma transmissão de dados rápida e eficiente, economizando tempo e aumentando a produtividade.
         </Article>
+        <img src={imagem4} alt="imagem representativa" className="img" />
+        </article>
+       
+        <article className={`
+        flex p-5 items-center justify-around gap-[10rem]
+        `} >
+             <Article 
+        title="Conexões seguras e privadas">    
+           Sua privacidade e segurança de dados são de extrema importância. O SmartConnect incorpora protocolos de criptografia integrados para garantir a transmissão de dados segura e privada. Você pode conectar seus dispositivos com confiança e transmitir informações confidenciais sem se preocupar com acesso não autorizado.
+        </Article>
+        <img src={imagem5} alt="imagem representativa" className="img" />
+        </article>
+        
       </Area>
     )
 }
