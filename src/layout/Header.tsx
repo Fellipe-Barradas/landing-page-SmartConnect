df7ff728d063e3cd7c5c065a3888727c282460e7
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-07-01 09:28:37 -0300

    Merged

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,2 +1,14 @@
 import Area from "./Area";
 import logo from "/SmartConnectLogo.png"
+
+export default function Header(){
+    return(
+    <Area className='flex justify-between items-center text-white'>
+        <img src={logo} alt="Logo do projeto" className=' w-16 h-16 ml-3'/>
+        <nav className='flex gap-5 mr-6 text-xl'>
+          <span className=' cursor-pointer hover:text-blue-500 transition-colors'>FEATURES</span>
+          <span className=' cursor-pointer hover:text-blue-500 transition-colors'>HOMEPAGE</span>
+        </nav>
+      </Area>
+    )
+}
